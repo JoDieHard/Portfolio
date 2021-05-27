6a85149a5a696012ccf1e1978f126bc6c80386ac
JoDieHard <josephbowmandesigns@gmail.com>
2021-05-27 14:25:23 +0100

    TypeWriter Plugin
    This commit will add a typing effect to the cover image text.
    
    ** Spent some time trying to get the cursor to remove after it had finished typing but couldn't figure it out.

--- js/plugins.js
+++ js/plugins.js
@@ -3,6 +3,38 @@
 ScrollReveal().reveal('.project', { delay: 250, interval: 8, reset: true });
 ScrollReveal().reveal('.contact', { delay: 250, reset: false });
 
+// TypeWriterJS Plugin Functions ------------------------------
+
+var app = document.getElementById('t1');
+ 
+var typewriterOne = new Typewriter(app, {
+  loop: false,
+  delay: 45,
+});
+ 
+typewriterOne
+  .start()
+  .pauseFor(50)
+  .typeString('My name is Joseph Bowman')
+  .pauseFor(300)
+  .deleteChars(11)
+  .typeString(' Bowman')
+  .pauseFor(500)
+
+//------------------------------------------
+
+var app = document.getElementById('t2');
+ 
+var typewriterTwo = new Typewriter(app, {
+  loop: false,
+  delay: 45,
+});
+ 
+typewriterTwo
+  .pauseFor(4000)
+  .typeString('I am a Web Developer and Graphic Designer')
+  .start()
+
 // Choreographer Plugin Functions ------------------------------
 
  let choreographer = new Choreographer({
