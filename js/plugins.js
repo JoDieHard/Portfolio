b8f700514ff7b53faf225db109b75dfa01163984
JoDieHard <josephbowmandesigns@gmail.com>
2021-05-28 15:06:26 +0100

    Added Animsition Plugin
    This commit will add page transition animations for each internal link.

--- js/plugins.js
+++ js/plugins.js
@@ -53,7 +53,7 @@ typewriterTwo
         selector: '.scroll-down-btn',
         type: 'scale',
         style: 'transform:translateY',
-        from: 0,
+        from: 0, 
         to: -210,
         unit: 'px'
       },
@@ -111,6 +111,39 @@ typewriterTwo
   window.addEventListener('scroll', () => {
     headerAnim.runAnimationsAt(window.pageYOffset)
   });
+
+
+
+// Custom Side Menu Code ------------------------------
+  $(document).ready(function() {
+    $(".animsition-overlay").animsition({
+      inClass: 'overlay-slide-in-top',
+      outClass: 'overlay-slide-out-top',
+      inDuration: 1500,
+      outDuration: 800,
+      linkElement: '.animsition-link',
+      // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+      loading: true,
+      loadingParentElement: 'body', //animsition wrapper element
+      loadingClass: 'animsition-loading',
+      loadingInner: '', // e.g '<img src="loading.svg" />'
+      timeout: false,
+      timeoutCountdown: 5000,
+      onLoadEvent: true,
+      browser: [ 'animation-duration', '-webkit-animation-duration'],
+      // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+      // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+      overlay : true,
+      overlayClass : 'animsition-overlay-slide',
+      overlayParentElement : 'body',
+      transition: function(url){ window.location.href = url; }
+    });
+  });
+
+
+
+
+
   
 
 
