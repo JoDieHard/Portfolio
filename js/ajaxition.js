b103edc4cadd95efcfe306acda7bda2554e251c5
JoDieHard <josephbowmandesigns@gmail.com>
2021-06-02 13:16:59 +0100

    General Updates & Improvements.
    This commit will do the following:
    • Adds a favicon image
    • Speeds up Page transition speed
    • Adds a project.
    • Improves layout.

--- js/ajaxition.js
+++ js/ajaxition.js
@@ -1,43 +1,48 @@
 
 $(' #menu').on('click', '.ajaxition-link', function( event ){
 
-    const link = $(this).attr('href');
-    const linkContent = link + ' .ajaxition';
-    const container = $('#pageContainer');
 
+        const link = $(this).attr('href');
+        const linkContent = link + ' .ajaxition';
+        const container = $('#pageContainer');
+        // const menu = $('#menu');
+        // const linkClicked = $(this);
 
+    container.delay( 50 ).queue(function(){   // Adds class to Transition Psuedo-Element 
+        container.addClass('in');
+        container.dequeue();
+        
+    });
 
-container.delay( 50 ).queue(function(){   // Adds class to Transition Psuedo-Element 
-    container.addClass('in');
-    container.dequeue();
-});
+    event.preventDefault();     // STOPS LINKS FROM GOING TO OTHER PAGES
+    console.log(link);          // THIS CHECKS THAT MY CODE IS SELECTING CURRENT LINK
 
-event.preventDefault();     // STOPS LINKS FROM GOING TO OTHER PAGES
-console.log(link);          // THIS CHECKS THAT MY CODE IS SELECTING CURRENT LINK
+    setTimeout (function() {
 
-setTimeout (function() {
+    // Removes current HTML in Document 
+    $('.ajaxition').children().detach();
+        // Loads new HTML to page
+        container.load(linkContent);
+        console.log(link + ' has been loaded.');  
+    }, 200);
 
-// Removes current HTML in Document 
-$('.ajaxition').children().detach();
-    // Loads new HTML to page
-    container.load(linkContent);
-    console.log(link + ' has been loaded.');  
-}, 1000);
+    // Removes class from Transition Psuedo Element
+    container.delay( 500 ).queue(function(){
+        container.removeClass('in');
+        container.dequeue();
+    });
 
-// Removes class from Transition Psuedo Element
-container.delay( 1010 ).queue(function(){
-    container.removeClass('in');
-    container.dequeue();
-});
+    // Re-Initialize Plugins
+    setTimeout (function() {
+        setupJsPlugins();
+        console.log('Plugins have been Reinitialized.');
 
-// Re-Initialize Plugins
-setTimeout (function() {
-    setupJsPlugins();
-    console.log('Plugins have been Reinitialized.');
-    if (link === 'https://joseph-bowman.netmatters-scs.co.uk/#contact-me') {
-    $('#pageContainer #contact-me')[0].scrollIntoView();
-}
-}, 1050);
+        if (link === 'https://joseph-bowman.netmatters-scs.co.uk/#contact-me') {
+        $('#pageContainer #contact-me')[0].scrollIntoView();
+        } else {
+            $('#top')[0].scrollIntoView();
+        }
+    }, 550);
 
 
 });
